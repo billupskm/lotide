e0106a2f6702e7a333e7413676326321de934cb6
Bilal Mohamed <bilal.km4@gmail.com>
2023-04-26 22:48:49 -0400

    Refactored code

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+
+countLetters("She sells seashells by the seashore");
+console.log(countLetters("She sells seashells by the seashore"));
+
