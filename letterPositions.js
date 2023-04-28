a0b21353e9eb6d83733851f35a660b2901d17f57
Bilal Mohamed <bilal.km4@gmail.com>
2023-04-27 20:08:24 -0400

    Scan  object to find key

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+    
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1 === arr2) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  let results = {};
+  sentence = sentence.toLowerCase();
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+
+
+
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions('hello'), {h:[0], e:[1], l:[2,3], o:[4]});
+
