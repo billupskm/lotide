ea9c01a9da9887c942eca38e07dc89ca216961e9
Bilal Mohamed <bilal.km4@gmail.com>
2023-06-10 23:39:32 -0400

    Refactored code, removed comments

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,3 @@
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-    
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-    
-//   return true;
-// };
-
-// const assertArraysEqual = function(arr1, arr2) {
-//   if (arr1 === arr2) {
-//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-//   }
-// };
-
 const letterPositions = function(sentence) {
   let results = {};
   sentence = sentence.toLowerCase();
@@ -36,9 +14,3 @@ const letterPositions = function(sentence) {
 };
 
 module.exports = letterPositions;
-
-
-
-// console.log(letterPositions("lighthouse in the house"));
-// assertArraysEqual(letterPositions('hello'), {h:[0], e:[1], l:[2,3], o:[4]});
-
