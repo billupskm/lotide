5904edf22b6fe99ae6cc662eb3c6c603bfb0e3fe
Bilal Mohamed <bilal.km4@gmail.com>
2023-06-10 23:33:47 -0400

    Refactored code, removed comments

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKey = (object, callback) => {
   for (let key in object) {
     if (callback(object[key])) {
@@ -15,24 +7,4 @@ const findKey = (object, callback) => {
   return undefined;
 };
 
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-
-const object = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-let actual = findKey(object, x => x.stars === 2);
-let expected = "noma";
-assertEqual(actual, expected);
+module.exports = findKey;
