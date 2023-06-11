1b53fef65eb23402d77aebc634f6f3d417586113
Bilal Mohamed <bilal.km4@gmail.com>
2023-06-10 23:48:16 -0400

    Refactored code, removed comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,35 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-        if (eqArrays(arr1[i], arr2[i]) === false) {
-          return false;
-        } else if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (arr1 === arr2) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-// Create function that takes elements until truthy value
-// For...in loop to iterate through array
-// Use slice to stop iteration at truthy value
-// Add empty array for new results
-
-
-
 const takeUntil = function(array, callback) {
   let result = [];
   for (let i in array) {
@@ -41,25 +9,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-
-
-
-
-
-
-
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-//EXPECTED OUTPUT
-// [ 1, 2, 5, 7, 2 ]
-// --
-// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+module.exports = takeUntil;
